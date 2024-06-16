6fd20c754866674695d5080fde9dd331cbb2b40c
applekerstie <applekerstie@gmail.com>
2024-06-16 21:50:58 +0900

    event key

---
+++ src/lecture/App_event_2.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="modifiers">
+    <div @click="clickDiv">
+      DIV 영역
+      <p @click="clickP">
+        P 영역
+        <span @click.stop="clickSpan">SPAN 영역</span>
+        <a href="https://naver.com" @click.prevent.stop="clickA">a 영역</a>
+      </p>
+    </div>
+  </div>
+
+  <a href="https://naver.com" @click.prevent="clickA">a 영역</a>
+
+</template>
+
+<script>
+export default {
+  setup () {
+    const clickDiv = () => {
+      console.log('clickDiv');
+      location.href = 'https://www.naver.com'
+    }
+    const clickP = () => {
+      console.log('clickP');
+    }
+    const clickSpan = () => {
+      console.log('clickSpan');
+      //e.stopPropagation();
+      alert('좋아요')
+    }
+    const clickA = () => {
+      //e.preventDefault();
+      alert('어떤기능');
+    }
+
+    return {
+      clickDiv,
+      clickP,
+      clickSpan,
+      clickA,
+    }
+  }
+}
+</script>
+
+<style scoped>
+#modifiers div,
+#modifiers p,
+#modifiers span {
+  padding: 40px;
+}
+#modifiers div {
+  background-color: #ccc;
+}
+#modifiers p {
+  background-color: #999;
+}
+#modifiers span {
+  background-color: #666;
+  display: 'block';
+}
+</style>
---
+++ src/lecture/App_event_key.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <input type="text" @keyup="addTodo1" />
+    <ul>
+      <li v-for="(todo, idx) in todos1" :key="idx">
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
+
+  <hr />
+  <div>
+    <input type="text" @keyup.enter="addTodo2" />
+    <ul>
+      <li v-for="(todo, idx) in todos2" :key="idx">
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue';
+
+export default {
+  setup () {
+    const todos1 = reactive([]);
+
+    const addTodo1 = (event) => {
+      //console.log( event.key );
+      if( event.key === 'Enter') {
+        todos1.push( event.target.value );
+      }
+    }
+
+    const todos2 = reactive([]);
+    const addTodo2 = (e) => {
+      todos2.push( e.target.value );
+      e.target.value = '';
+      e.target.focus();
+    }
+
+    return {
+      todos1,
+      addTodo1,
+      todos2,
+      addTodo2,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
