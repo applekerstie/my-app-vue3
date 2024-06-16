5794d5eda7349a221449887e31372eb1cc33f2d6
applekerstie <applekerstie@gmail.com>
2024-06-16 19:50:21 +0900

    for

---
+++ src/lecture/App_v-for.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <ul>
+      <template v-for="(item,idx) in evenItems" :key="item.id">
+        <!--<li v-if="item.id % 2 === 0">-->
+        <li>{{ item.id }} {{ item.message }}</li>
+      </template>
+    </ul>
+    <hr />
+    <ul>
+      <li v-for="(value, key, idx) in myObject" :key="key">
+        {{ value }} {{ key }} {{ idx }} 
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { computed, reactive } from 'vue';
+
+export default {
+  setup () {
+    const items = reactive([
+      { id:1, message:'Java'},
+      { id:2, message:'HTML'},
+      { id:3, message:'CSS'},
+      { id:4, message: 'JavaScript'},
+    ]);
+
+    const evenItems = computed( ()=>{
+      return items.filter(item => item.id %2 === 0)
+    });
+
+    const myObject = reactive({
+      title: '제목',
+      author: '홍길동',
+      publishedAt: '2016-01-01',
+    })
+
+    return { 
+      items,
+      evenItems,
+      myObject
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
