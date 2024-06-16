40975d8a09709745f7aa10f44d7208c1efb30327
applekerstie <applekerstie@gmail.com>
2024-06-16 17:10:52 +0900

    readonly

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,19 @@
 <template>
   <div>
-    <p>author: {{ author }}</p>
-    <p>title: {{ title }}</p>
+
   </div>
 </template>
 
 <script>
-import { reactive, toRef, toRefs } from 'vue';
-
 export default {
   setup () {
-    const book = reactive({
-      author: 'Vue Team',
-      year: '2020',
-      title: 'Vue 3 Guide',
-      description: '당신은 지금 바로 이 책을 읽습니다',
-      price: '무료',
-    });
+    
 
-    //const { author, title } = book;
-    // 반응성을 잃지 않으면서 구조할당 하기
-    //const { author, title } = toRefs(book);
-
-    // 반응성 잃지 않으면서 하나씩 할당하기
-    const author = toRef(book, 'author');
-    const title = toRef(book, 'title');
-
-    return {
-      author, title, book
-    }
+    return {}
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
