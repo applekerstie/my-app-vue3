dba42c8243f42595184fdb97438d78e162b53be1
applekerstie <applekerstie@gmail.com>
2024-06-16 18:41:45 +0900

    style binding

---
+++ src/lecture/App_style_binding.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :style="styleObject">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla tempora repellat, saepe nostrum error aperiam, deleniti provident, aut quae rem reiciendis quasi asperiores fugit! Nisi iste accusamus quis cumque provident.
+  </div>
+  <button v-on:click="fontSize++">+</button>
+  <button v-on:click="fontSize--">-</button>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+
+export default {
+  setup () {
+    /*
+    const styleObject = {
+      fontSize : '13px',
+      color: 'red',
+    }
+    */
+    const fontSize = ref(13);
+    const color = ref('red');
+
+    const styleObject = computed( ()=>{
+      return {
+        fontSize: fontSize.value + 'px',
+        color: color.value,
+      }
+    });
+
+    return {
+      styleObject,
+      fontSize,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
