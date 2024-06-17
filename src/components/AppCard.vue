9320efb7bc592775d210420ff8b5aea02414db53
applekerstie <applekerstie@gmail.com>
2024-06-17 13:57:56 +0900

    style module

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
+      <h5 class="card-title" :class="$style.red">Card title</h5>
       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
       <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
@@ -9,16 +9,23 @@
 </template>
 
 <script>
-console.log('TheCard module');
+import { useCssModule } from 'vue'; 
+
+console.log('AppCard module');
 export default {
   setup () {
-    console.log('TheCard setup');
+    console.log('AppCard setup');
+
+    const style = useCssModule();
+    console.log('style:', style);
 
     return {}
   }
 }
 </script>
 
-<style scoped>
-
+<style module>
+.red {
+  color: red;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-//import TheCard from './components/TheCard.vue';
+//import AppCard from './components/AppCard.vue';
 
 const app = createApp(App);
-//app.component('TheCard', TheCard);
+//app.component('AppCard', AppCard);
 app.mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
