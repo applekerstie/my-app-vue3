75f0858925d525294bad57f9ea2744b0a17dc59e
applekerstie <applekerstie@gmail.com>
2024-06-17 13:35:56 +0900

    add components

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+//import TheCard from './components/TheCard.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+//app.component('TheCard', TheCard);
+app.mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
